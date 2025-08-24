6a344a7671a67a08e991832ffaf7dcd68f8afd47
Guest-22 <propisyunalhakir@gmail.com>
2025-08-24 14:19:33 +0800

    Add some comment

--- src/main/java/com/mycompany/oopdemo/Motorcycle.java
+++ src/main/java/com/mycompany/oopdemo/Motorcycle.java
@@ -2,7 +2,7 @@ package com.mycompany.oopdemo;
 
 // ----------------------------------------------------------------------------------------
 // Inheritance:     This child class inherits all of the attributes and methods of the parent class.
-// Polymorphism:    Poly = mutliple, Morph = forms; The ability of an object or method to take on many forms.
+// Polymorphism:    Poly = mutliple, Morph = forms; The ability of an object to take on many forms.
 //                  Overriding methods are common in abstract classses and interfaces.
 // ----------------------------------------------------------------------------------------
 
